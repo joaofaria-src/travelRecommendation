9dea178a53ea8d9c3183b4d2f3ff38b13f8b5a7a
joaofaria-src <17826036+joaofaria-src@users.noreply.github.com>
2024-11-20 06:31:44 -0500

    final adjustments

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -78,7 +78,7 @@ document.getElementById("clearButton").addEventListener("click", clearHandler);
 
 //hide the searchbar when not on homepage
 const searchBar = document.querySelector('.navbar-search');
-if (!window.location.pathname.endsWith('index.html')) {
+if (!window.location.pathname.endsWith('index.html') || ('travelRecommendation')) {
     searchBar.style.display = 'none';
   }
 
